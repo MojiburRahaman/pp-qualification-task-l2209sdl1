8e2259ef2806a133dbf7c163cb5ca21500f053ed
MojiburRahaman <santo68683@gmail.com>
2022-10-07 23:18:12 +0600

    Route documentation added

--- routes/admin.php
+++ routes/admin.php
@@ -7,18 +7,25 @@ use App\Http\Controllers\Admin\DashboardController;
 
 Route::middleware('guest:admin')->group(function () {
 
+    // for admin login
     Route::get('login', [AdminAuthController::class, 'AdminLogin'])->name('AdminLogin');
     Route::post('login', [AdminAuthController::class, 'AdminLoginPost'])->name('AdminLogin.Post');
 
+    // for admin register
     Route::get('register', [AdminAuthController::class, 'AdminRegister'])->name('AdminRegister');
     Route::post('register', [AdminAuthController::class, 'AdminRegisterPost'])->name('AdminRegister.Post');
 });
 
 Route::middleware('auth:admin')->group(function () {
+    // logout route
     Route::post('logout', [AdminAuthController::class, 'AdminLogOut'])->name('AdminLogOut');
-    
+
+    // dashborad route
     Route::get('dashboard', [DashboardController::class, 'DashboardView'])->name('BackendDashboardView');
+
+    // for all transaction  route
     Route::get('transactions', [DashboardController::class, 'AllTranaction'])->name('AllTranaction');
 
+    // account type create,edit,update route
     Route::resource('accounttype', AccountTypeController::class);
 });
--- routes/web.php
+++ routes/web.php
@@ -7,26 +7,26 @@ use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\Frontend\UserProfileController;
 use Illuminate\Support\Facades\Route;
 
-
+// home view route
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-Route::get('/register', function () {
-    return redirect()->route('LoginView');
-})->name('RegisterView')->middleware('guest:web');
 
+// frontend login and register route
+Route::get('/register', function () {
+    return redirect()->route('login');
+})->name('RegisterView')->middleware('guest:web');
 Route::get('/login', [AuthController::class, 'LoginView'])->name('login')->middleware('guest:web');
 Route::post('/login', [AuthController::class, 'LoginPost'])->name('LoginPost')->middleware('throttle:5,360');
 Route::post('/register', [AuthController::class, 'RegisterPost'])->name('RegisterPost')->middleware('guest:web');
 
+// for code verify route
 Route::get('/code', [AuthController::class, 'AuthCodeVerify'])->name('AuthCodeVerify')->middleware('auth:web');
 Route::post('/code', [AuthController::class, 'AuthCodeVerifyPost'])->name('AuthCodeVerifyPost')->middleware('auth:web');
 
 Route::middleware('auth:web', 'authremember')->group(function () {
-
-
     Route::middleware('AgentAccount')->group(function () {
-        // for cash in to personal account from agent
+        // for cash in to personal account from agent accounbt
         Route::get('/cash-in', [AgentTransactionController::class, 'CashInView'])->name('CashInView');
         Route::post('/cash-in', [AgentTransactionController::class, 'CashInPost'])->name('CashInPost');
     });
@@ -43,11 +43,15 @@ Route::middleware('auth:web', 'authremember')->group(function () {
         Route::get('/cashout', [PersonalAccountTransactinController::class, 'CashOutView'])->name('CashOutView');
         Route::post('/cashout', [PersonalAccountTransactinController::class, 'CashOutViewpost'])->name('CashOutViewpost');
     });
-
+// for user admin panel view
     Route::get('/dashboard', [FrontendController::class, 'DashboardView'])->name('DashboardView');
+    
+    // for view user transaction
     Route::get('/transaction', [FrontendController::class, 'TransactionView'])->name('TransactionView');
-
+    
+    // for view user profile 
     Route::get('/profile', [UserProfileController::class, 'ProfileView'])->name('ProfileView');
-
+    
+    // for view user logout
     Route::post('/logout', [AuthController::class, 'Logout'])->name('Logout');
 });
